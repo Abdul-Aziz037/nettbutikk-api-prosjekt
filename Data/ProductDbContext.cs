d4820da79808a6daa4316f0c07f9db84a47d64ea
Abdul-Aziz037 <Abdul-Aziz.Mohammed@gokstadakademiet.no>
2024-04-16 15:55:40 +0200

    la til modellen med entityframework

---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nettbutikk_api.Models.Entities;
+
+namespace nettbutikk_api.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        // tabellen lages
+        public DbSet<Product> products { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using nettbutikk_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DbMysql
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 21))));
+
 
 var app = builder.Build();
 
