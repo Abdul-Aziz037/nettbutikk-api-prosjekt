362d92d2a70f303bbfb6040c3db8d927cc975516
Abdul-Aziz037 <Abdul-Aziz.Mohammed@gokstadakademiet.no>
2024-04-18 11:46:59 +0200

    lagt til controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -8,16 +8,33 @@ namespace nettbutikk_api.Controllers;
 [ApiController]
 public class ProductsController : ControllerBase
 {
-    //[HttpGet]
-    //public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
-    //{
-    //    var products = await _productService.GetProductsAsync();
+    /*
+    private readonly IProductService _productService;
 
-    //    if (products == null)
-    //    {
-    //        return NotFound(); // Returnerer 404 Not Found hvis produkter ikke ble funnet
-    //    }
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+       var products = await _productService.GetProductsAsync();
 
-    //    return Ok(products);
-    //}
+       if (products == null)
+       {
+           return NotFound();
+       }
+       return Ok(products);
+    }
+
+    [HttpGet("{productId}", Name = "GetProductId")]
+    public async Task<ActionResult<ProductDTO>> GetPostByIdAsync(int productId)
+    {
+        var rest = await _productService.GetPostByIdAsync(postId);
+        return rest != null ? Ok(rest) : NotFound("Fant ikke product");
+    }
+
+
+
+    */
 }
